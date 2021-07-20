7c1b4af3084e82a4b99085d4298fb61d46a640d0
MichaelABartlett <michael@machinistchoice.com>
2021-07-20 16:09:53 -0500

    saving progress turning in, still needs work

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 
 const printBoard = () =>  {
   for (let i = 0; i < board.length; i++) {
-    console.log(board[i],"what is this");
+    console.log(board[i]);
   }
 }
 
@@ -31,58 +31,95 @@ const getRandomInt = (min, max) => {
 const generateHint = (guess) =>  {
   // your code here
   let solutionArray = solution.split('');
-  console.log(`Solution array is: ${solutionArray}`);
+  //console.log(`Solution array is: ${solutionArray}`);
   let guessArray = guess.split('');
-  console.log(`Guess array is: ${guessArray}`);
+  //console.log(`Guess array is: ${guessArray}`);
   //console.log(guessArray[1]);
   let correctLetterLocations = 0;
-  
   let correctLetters = 0;
-  let targetIndex = -1;
-  for(let i = 0; i < solutionArray.length; i++)
+  let correctLocationString = "";
+  let correctLetterString = "";
+  
+ 
+  
+// ************** checking correct letter and location *******************
+
+  for(let i = 0; i < solutionArray.length; i++){
     if(guessArray[i] === solutionArray[i]){
-      console.log('there is a correct location match');
+      //console.log('there is a correct location match');
       correctLetterLocations += 1;
-      console.log("correct letter locations: ", correctLetterLocations);
+      //console.log("correct letter locations: ", correctLetterLocations);
       solutionArray[i] = null;
-      console.log("new solutionArray correct location match: ", solutionArray);
-      return solutionArray.join();
+      //console.log("new solutionArray correct location match: ", solutionArray);
+      //correctLocationString = solutionArray.join();
 
     } 
-  for(let i = 0; i < solutionArray.length; i++)
-    if(solutionArray.indexOf(guessArray[i]) >= 0){
-      console.log("There is a letter match");
-      correctLetters += 1;
-      targetIndex = i;
-      solutionArray[targetIndex] = null;
-      console.log("targetIndex: ", targetIndex);
-      console.log("new solutionArray letter match: ", solutionArray);
-      return solutionArray.join();
+   
+  }
+ // ********** checking correct letter  *******************   
+  for(let i = 0; i < solutionArray.length; i++){
+    //console.log('is this running');
+    //console.log('solutingArray in check letter: ', solutionArray)
+    let targetIndex = solutionArray.indexOf(guessArray[i]);
+    //console.log('targetIndex: ', targetIndex);
+  if(targetIndex > -1){
+    //console.log("There is a letter match");
+    
+    correctLetters += 1;
+    solutionArray[targetIndex] = null;
+    //console.log("targetIndex: ", targetIndex);
+    //console.log("new solutionArray letter match: ", solutionArray);
+    //correctLetterString = solutionArray.join();
+    
     }
-  
-    // fruits.splice(1, 1, null,);
+    
+  }
+     
+  //console.log('correctLocationString: ', correctLocationString);
+  //console.log('correctLetterString: ', correctLetterString);
+  let boardString = (guess+","+correctLetterLocations.toString()+"-"+correctLetters.toString());
+  board.push(boardString);
+
+
+  //console.log('this the the new board: ', board);
 
 }
+
+let continueGame;
+let count = 1;
 
 const mastermind = (guess) => {
   solution = 'abcd'; // Comment this out to generate a random solution
   // your code here
-  
-  if(guess == solution){
-    console.log("guess: ", guess);
-    console.log("solution: ", solution);
-    return console.log('you guessed it');
-  }else {
     generateHint(guess);
+  if(guess == solution){
+    printBoard();
+    console.log('you guessed it');
+    continueGame = false;
+  }else if(count >= 3){
+    console.log('You ran out of turns! The solution was ', solution)
+    continueGame = false;
+  } else {
+    console.log("Game Board");
+    printBoard();
+    console.log('Try again')
   }
+
 }
 
 
 const getPrompt = () =>  {
   rl.question('guess: ', (guess) => {
     mastermind(guess);
-    printBoard();
-    getPrompt();
+    if(continueGame = false){
+      console.log('game over')
+      return false;
+    } else {
+     
+      count += 1;
+      getPrompt();
+    }
+    
   });
 
 }
